c739dc2084670c54666a8465b5f201af0a26e8c8
Fuady Rosma Hidayat <fuady@live.com>
2024-06-14 10:23:07 +0700

    Add project files.

---
+++ src/NetflixKw.ConsoleApp/Car.cs
@@ -0,0 +1,7 @@
+﻿namespace NetflixKw.ConsoleApp;
+
+public class FufuCar
+{
+    public string Brand { get; set; } = "Ferrari";
+    public string Color { get; set; } = "Red";
+}
---
+++ src/NetflixKw.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace NetflixKw.ConsoleApp;
+
+public static class FufuProgram
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        var inputanUser = Console.ReadLine();
+
+        if (inputanUser == "a")
+        {
+            Console.WriteLine("Inputan user adalah a");
+        }
+        else if (inputanUser == "b")
+        {
+            Console.WriteLine("Inputan user adalah b");
+        }
+        else
+        {
+            Console.WriteLine("Inputan user bukan a atau b");
+        }
+
+        var abc = 7;
+        var xyz = 9;
+
+        Console.WriteLine($"Isi abc: {abc}");
+        Console.WriteLine($"Isi xyz: {xyz}");
+    }
+}
